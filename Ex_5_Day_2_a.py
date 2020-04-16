633617220ae7d96995e00b7f0658f082d99e7415
telegrace <47720604+telegrace@users.noreply.github.com>
2020-04-16 21:32:06 +0200

    Create Ex_5_Day_2_a.py

---
+++ Ex_5_Day_2_a.py
@@ -0,0 +1,16 @@
+my_name = "Grace"
+my_age = 45 # yes it is a lie
+my_weight = 60 # kg
+my_height = 165 # cm
+my_eyes = "brown"
+my_teeth = "tanned"
+
+print(f"Lets talk about {my_namee}.")
+print(f"She's {my_age} old.")
+print("But we all know that's a lie.")
+print(f"She weighs {my_weight} kg.")
+print(f"She is {my_height} cm tall.")
+print(f"She has {my_eyes} eyes and her teeth are '{my_teeth}'.")
+
+total = my_height / my_weight + my_age
+print(f"If I divide {my_name}'s height, {my_height}, by her weight, {my_weight}, and then add her {my_age} the total is {total}.")
